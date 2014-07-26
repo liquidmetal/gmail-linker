5429d0c556a82a1b34a345cea916d67995e35fdc
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2014-07-26 15:52:53 +0530

    Link tickets in gchat

--- chrome/linker.js
+++ chrome/linker.js
@@ -3,13 +3,40 @@ function parseEmail() {
     emailContents = gmail.dom.email_contents();
 }
 
+function linkChatPiece(index) {
+    var pieceHtml = $(this).html();
+    m = pieceHtml.match(/[a-zA-Z]+\-[0-9]+/g)
+    for(var i=0; i<m.length; i++) {
+        pieceHtml = pieceHtml.replace(m[i], "<a target='_blank' href='http://jira.anim.dreamworks.com/browse/" + m[i] + "'>" + m[i] + "</a>");
+    }
+    $(this).html(pieceHtml);
+}
+
+function linkifyChats(chatWindows) {
+    // The div that contains the display pic + text
+    chatContexts = $('.no > .nH.nn > .AD > .nH > .nH.Hd > .nH > .nH > .nH.ko.aXjCH > .Z8Dgfe > .kf');
+
+    // Just the texts (chat on right)
+    chatTextsRight = $('.aec > .kl', chatContexts);
+
+    // Just the texts (chat on left)
+
+    // hanging text
+    chatTextsLeft = $('.km > .kk > span:not([class])', chatContexts)
+    chatTextsLeftHanging = $('.km > .kl', chatContexts)
+
+    $.each(chatTextsRight, linkChatPiece);
+    $.each(chatTextsLeft, linkChatPiece);
+    $.each(chatTextsLeftHanging, linkChatPiece);
+}
+
 function initialize() {
     window.gmail = Gmail()
     console.log("Logged in from: " + window.gmail.get.user_email());
     console.log("Is threaded: " + window.gmail.check.is_thread());
     window.gmail.observe.on('chat_open', function(id, url, body) {
-      console.log("id:", id, "url:", url, 'body', body);
-      window.gmail.chat.chat_windows();
+      chatWindows = window.gmail.chat.chat_windows();
+      linkifyChats(chatWindows);
       });
     window.gmail.observe.on('chat_close', function(id, url, body) {
       console.log("id:", id, "url:", url, 'body', body);
